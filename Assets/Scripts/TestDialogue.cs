5ced02bccd5dbe555c0ea159da0a051255fcb211
Aducj1910 <adish1910@gmail.com>
2020-12-08 13:02:19 +0530

    helo

---
+++ Assets/Scripts/TestDialogue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestDialogue : MonoBehaviour
+{
+
+    [SerializeField]public string sentence;
+    public Text dialogueText;
+
+    public void SendSignal()
+    {
+       dialogueText.text = sentence;
+    }
+    public void RevertSignal()
+    {
+        dialogueText.text = "";
+    }
+}
